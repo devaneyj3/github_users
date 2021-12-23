f415692cb835f11e2ccfb4302a32b6f7ccd188df
Jordan Devaney <jordandevaney28@gmail.com>
2021-12-23 17:34:23 -0500

    set up context

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Info, Repos, User, Search, Navbar } from "../components";
 import loadingImage from "../images/preloader.gif";
-import { GithubContext } from "../context/context";
+import { useGlobalContext } from "../context/context";
 const Dashboard = () => {
+	const data = useGlobalContext();
+	console.log(data);
 	return (
 		<main>
 			<Navbar />
