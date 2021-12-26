8cc27f96faf39fabd38a5f32a00dd2762f2776a7
Jordan Devaney <jordandevaney28@gmail.com>
2021-12-26 15:47:56 -0500

    calculate most popular language and put in doughnut chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,29 +5,37 @@ import { Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
 	const { repos } = useGlobalContext();
 	let languages = repos.reduce((total, item) => {
-		const { language } = item;
+		const { language, stargazers_count } = item;
 		if (!language) return total;
 		if (!total[language]) {
-			total[language] = { label: language, value: 1 };
+			total[language] = { label: language, value: 1, stars: stargazers_count };
 		} else
 			total[language] = {
 				...total[language],
 				value: total[language].value + 1,
+				stars: total[language].stars + stargazers_count,
 			};
 		return total;
 	}, {});
+
 	languages = Object.values(languages)
 		.sort((a, b) => {
-			return a - b;
+			return b.value - a.value;
 		})
 		.slice(0, 5);
 	console.log(languages);
+	const mostPopular = Object.values(languages)
+		.sort((a, b) => {
+			return b.stars - a.stars;
+		})
+		.map((item) => {
+			return { ...item, value: item.stars };
+		});
 	return (
 		<section className="section">
 			<Wrapper className="section-center">
 				<Pie3D data={languages} />;<div></div>
-				<Doughnut2D data={languages} />
-				<div></div>
+				<Doughnut2D data={mostPopular} />;<div></div>
 			</Wrapper>
 		</section>
 	);
